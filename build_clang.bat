305d047e1d00efb76b534661b32fb21e44ca564b
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2018-01-03 15:42:52 +0100

    LICENSE added

--- build_clang.bat
+++ build_clang.bat
@@ -1,2 +1,2 @@
 @echo off
-clang -Wall -Wpedantic -Wno-deprecated -Wno-int-conversion base64.c base64_app.c -o base64.exe
+clang -O3 -Wall -Wpedantic -Wno-deprecated -Wno-int-conversion base64.c base64_app.c -o base64.exe
