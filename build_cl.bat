5bb98100153a0c66ffd6a80411baf78c53bbf8fe
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2018-03-29 21:19:04 +0200

    update build scripts

--- build_cl.bat
+++ build_cl.bat
@@ -1,3 +1,2 @@
 @echo off
-cl /W3 /GS /Gd /Zi /Od /Ob0 /MDd /TC /Fe: base64.exe /Fd: base64.pdb base64.c base64_app.c
-del *.ilk *.obj
+vcvarsall & cl /nologo /D_CRT_SECURE_NO_WARNINGS /W3 /GS /Gd /Zi /Od /Ob0 /MDd /TC /Fe: base64.exe /Fd: base64.pdb base64.c base64_app.c & del *.ilk *.obj
--- build_clang.bat
+++ build_clang.bat
@@ -1,2 +1,2 @@
 @echo off
-clang -O3 -Wall -Wpedantic -Wno-deprecated -Wno-int-conversion base64.c base64_app.c -o base64.exe
+clang -O3 -Wall -Wpedantic -Wno-deprecated base64.c base64_app.c -o base64.exe
