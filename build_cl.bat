a39bc39fb08c5485c53af8f1d4bfe3b8ed0824ff
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2018-09-15 22:47:58 +0200

    remove old files

--- build_cl.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-vcvarsall & cl /nologo /D_CRT_SECURE_NO_WARNINGS /W3 /GS /Gd /Zi /Od /Ob0 /MDd /TC /Fe: base64.exe /Fd: base64.pdb base64.c base64_app.c & del *.ilk *.obj
--- build_clang.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-clang -O3 -Wall -Wpedantic -Wno-deprecated base64.c base64_app.c -o base64.exe
